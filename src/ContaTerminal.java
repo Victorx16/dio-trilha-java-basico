d49de8c72fa4c95ba9f20cf858a440e54b992ea2
Victor Xavier <victorxavi418@gmail.com>
2025-05-16 17:16:19 -0300

    Desafio DIO criar conta banco interativa finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-        double saldo = 237.48;
+        public static void main(String[] args) throws Exception {
+                Scanner scanner = new Scanner(System.in);
+
+                double saldo = 237.48;
+
+                System.out.println("Informe sua conta: ");
+                int numero = scanner.nextInt();
+
+                System.out.println("Informe sua agência: ");
+                String agencia = scanner.next();
+
+                System.out.println("Informe seu nome e sobrenome: ");
+                String nomeCliente = scanner.nextLine();
+
+                System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                        ", conta " + numero + ", e seu saldo " + saldo + " já está disponível para saque.");
+
+
+                scanner.close();
+        }
+
 
 
 }
